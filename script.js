f37222c23138fa47ba8a7485ef3dd88b71ac4d49
Bradley Morin <bradleymorin@gmail.com>
2024-11-11 18:10:31 -0700

    Add victory conditions
    After a valid move each row, column, and diagonal is checked
    for three of the same pieces. In the event of a winner or tie,
    the result is passed up to the ScreenController via callback.

--- script.js
+++ script.js
@@ -55,17 +55,101 @@ function GameController(playerOneName = "Player One", playerTwoName = "Player Tw
 
   const getActivePlayer = () => activePlayer;
 
-  const playRound = (row, column) => {
+  const playRound = (row, column, onVictoryOrTie) => {
     const gameboard = board.getBoard();
     // Guard clause to handle clicks on already claimed cells.
     if (gameboard[row][column].getValue() !== "") return;
 
     board.claimCell(row, column, getActivePlayer().token);
-
-    // Todo: check victory condition.
-
+    checkVictoryAndHandle(onVictoryOrTie);
     switchPlayerTurn();
   };
+
+  const checkVictoryAndHandle = (callback) => {
+    const gameboard = board.getBoard();
+    const winner = checkForWinner(gameboard);
+    const isTie = isBoardFull(gameboard);
+
+    if (winner !== null) {
+      callback(winner.name + " wins!");
+    } else if (isTie) {
+      callback("It's a tie!");
+    }
+  }
+
+  const checkForWinner = (gameboard) => {
+    let winner = null;
+    let winningRow = null;
+    let winningColumn = null;
+    let winningDiagonal = null;
+    // Check rows for a winner.
+    for (let i = 0; i < 3; i++) {
+      if (
+        (gameboard[i][0].getValue() !== "") &&
+        (gameboard[i][0].getValue() === gameboard[i][1].getValue()) &&
+        (gameboard[i][0].getValue() === gameboard[i][2].getValue())
+      ) {
+        winningRow = i;
+      }
+    }
+    // Check columns for a winner.
+    for (let j = 0; j < 3; j++) {
+      if (
+        (gameboard[0][j].getValue() !== "") &&
+        (gameboard[0][j].getValue() === gameboard[1][j].getValue()) &&
+        (gameboard[0][j].getValue() === gameboard[2][j].getValue())
+      ) {
+        winningColumn = j;
+      }
+    }
+    // Check diagonals
+    if (
+      (gameboard[0][0].getValue() !== "") &&
+      (gameboard[0][0].getValue() === gameboard[1][1].getValue()) &&
+      (gameboard[0][0].getValue() === gameboard[2][2].getValue())
+    ) {
+      winningDiagonal = 1;
+    }
+    else if (
+      (gameboard[0][2].getValue() !== "") &&
+      (gameboard[0][2].getValue() === gameboard[1][1].getValue()) &&
+      (gameboard[0][2].getValue() === gameboard[2][0].getValue())
+    ) {
+      winningDiagonal = 2;
+    }
+    let winningToken;
+    if (winningRow !== null) {
+      winningToken = gameboard[winningRow][0].getValue();
+    }
+    else if (winningColumn !== null) {
+      winningToken = gameboard[0][winningColumn].getValue();
+    }
+    else if (winningDiagonal !== null) {
+      winningToken = gameboard[1][1]
+    }
+    if (winningToken !== null) {
+      if (winningToken === "X") {
+        winner = players[0];
+      }
+      else if (winningToken === "O") {
+        winner = players[1];
+      }
+    }
+    return winner;
+  }
+
+  const isBoardFull = (gameboard) => {
+    let isFull = true;
+    // Check if each cell has a token.
+    gameboard.forEach((row, rowIndex) => {
+      row.forEach((column, columnIndex) => {
+        if (gameboard[rowIndex][columnIndex].getValue() === "") {
+          isFull = false;
+        }
+      });
+    });
+    return isFull;
+  }
 
   return {
     playRound,
@@ -109,7 +193,15 @@ function ScreenController() {
     // Guard clause to make sure the user clicked a cell.
     if (!row) return;
 
-    game.playRound(row, column);
+    game.playRound(row, column, (result) => {
+      if (result) {
+        // Delay the alert slightly so that the screen updates before the alert is sent.
+        setTimeout(() => {
+          alert(result);
+        }, 1);
+        boardDiv.removeEventListener('click', clickHandlerBoard);
+      }
+    });
     updateScreen();
   }
   boardDiv.addEventListener('click', clickHandlerBoard);
